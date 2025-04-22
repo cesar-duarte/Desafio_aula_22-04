958243725b64843177d3f6beb8063f8232f6f628
Cesar-Duarte <142168536+cesar-duarte@users.noreply.github.com>
2025-04-22 20:47:43 -0300

    teste 2

--- Main.c
+++ Main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    printf("olá, Github Actions!\n");
+    
+    int n1, 2, soma;
+    printf("Digite o primeiro número: ");
+    scanf("%d", 1);
+    printf("Digite o segundo número: ");
+    scanf("%d", 2);
+
+    soma = n1 + 2;
+
+    printf("A soma é: %d\n", soma);
     return 0;
+    #include <stdio.h>
 }
--- teste.c
+++ teste.c
@@ -2,7 +2,7 @@
 #include <assert.h>
 
 void testar() {
-  assert(1==1);
+  assert(1!=0);
   printf("Teste passou!\n");
 }
 
