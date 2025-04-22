08e4182541114e65e6108fc9fc20706da483f6b8
Cesar-Duarte <142168536+cesar-duarte@users.noreply.github.com>
2025-04-22 20:49:33 -0300

    Teste 3

--- Main.c
+++ Main.c
@@ -2,13 +2,13 @@
 
 int main() {
     
-    int n1, 2, soma;
+    int soma;
     printf("Digite o primeiro número: ");
     scanf("%d", 1);
     printf("Digite o segundo número: ");
     scanf("%d", 2);
 
-    soma = n1 + 2;
+    soma = ;
 
     printf("A soma é: %d\n", soma);
     return 0;
